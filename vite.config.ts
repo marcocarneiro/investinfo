2b8682830fe861a1154b09f2dc29dcf175d40e16
marcocarneiro <marco.carneiro7@gmail.com>
2026-04-02 17:19:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bolsa-verde-azul/" : "/",
+  base: "/bolsa-verde-azul/",
   server: {
     host: "::",
     port: 8080,
